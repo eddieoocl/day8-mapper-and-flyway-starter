aade93341aaff5bc704173f705d3b6d1ab702622
Minghao Li <minghao.li@thoughtworks.com>
2024-11-24 17:34:08 +0800

    [SpringBoot][JPA] fix: add Enumerated for gender to convert string

--- src/main/java/com/oocl/springbootemployee/model/Employee.java
+++ src/main/java/com/oocl/springbootemployee/model/Employee.java
@@ -1,6 +1,8 @@
 package com.oocl.springbootemployee.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,6 +14,7 @@ public class Employee {
     private Integer id;
     private String name;
     private Integer age;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
     private Double salary;
     private Boolean isActive = true;
